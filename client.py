8fe38f21bc6c098f3adbc47cf9a549b51b7721dc
vancent <guohaonk@sina.com>
2015-03-26 17:05:44 +0800

    ls

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from socket import *
 
 HOST = '127.0.0.1'
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 
 from SocketServer import (TCPServer as TCP, StreamRequestHandler as SRH)
 from time import ctime
